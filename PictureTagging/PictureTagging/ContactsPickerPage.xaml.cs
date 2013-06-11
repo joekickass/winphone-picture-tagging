6632a75c5c4994a8523d6781063df9b4694a646d
joekickass <tnilsson.x@gmail.com>
2013-06-11 10:58:30 +0200

    Added capability

--- PictureTagging/PictureTagging/ContactsPickerPage.xaml.cs
+++ PictureTagging/PictureTagging/ContactsPickerPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace PictureTagging
+﻿using System.Windows.Navigation;
+
+namespace PictureTagging
 {
     public partial class ContactsPickerPage
     {
         public ContactsPickerPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
         }
     }
 }
